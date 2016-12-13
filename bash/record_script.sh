500a0d9a8af18c507f3188d8f77d3a01f9419e64
evan.kim <evan.kim@kaist.ac.kr>
2016-12-13 09:58:25 +0900

    Fix the record file path

--- bash/record_script.sh
+++ bash/record_script.sh
@@ -4,5 +4,5 @@ sudo sh stop_script.sh
 #161018_1: avconv -ar 44100 -ac 2 -f s16le -i /dev/zero -f video4linux2 -s 640x360 -r 10 -i /dev/video0 -f flv -c:v libx264 -c:a copy "rtmp://a.rtmp.youtube.com/live2/ej6m-f5uj-52qt-bsz9"
 #161018_2 avconv -ar 44100 -ac 2 -f s16le -i /dev/zero -f video4linux2 -s 426X240 -r 30 -i /dev/video0 -f flv -c:v libx264 -g 2 -keyint_min 2 -c:a libmp3lame "rtmp://a.rtmp.youtube.com/live2/73mv-4v9f-x063-ezep"
 #raspivid -w 1920 -h 1080 -fps 24 -t 5000 -o /var/www/html/vid_files/video.h264
-raspivid -md 5 -t 5000 -o /var/www/html/vid_files/video.h264
+raspivid -md 5 -t 5000 -o /var/www/html/files/video.h264
 #echo "Recording is done .."
