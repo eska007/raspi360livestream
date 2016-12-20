41a604dd8d1917b5892ff7745c7ba83680fba6f0
evan.kim <evan.kim@kaist.ac.kr>
2016-12-20 11:47:16 +0900

    Add dewarped image resize

--- python/cv_image_process.py
+++ python/cv_image_process.py
@@ -48,23 +48,29 @@ def set_cv_frame_res(cap, x,y):
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, int(x))
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, int(y))
     return str(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),str(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
 cap = cv2.VideoCapture(0)
 ws, hs = set_cv_frame_res(cap, 1280, 720)
 print ("Set frame resolution: " + ws + ',' + hs)
-#cam = Camera(0, {"width": 1280, "height": 720})
+
 ext = '.avi'
 codec = 'XVID' #OK
+
 # OPENCV 3.1.0
 fourcc = cv2.VideoWriter_fourcc(*codec)
-out = cv2.VideoWriter('/var/www/html/vid_files/' + codec + ext, fourcc, 1, (1244, 107)) # O.K
-filename = '/var/www/html/vid_files/stream.png'
+out = cv2.VideoWriter('/var/www/html/files/' + codec + ext, fourcc, 1, (1244, 107)) # O.K
+filename = '/var/www/html/files/stream.png'
+
 while(True):
     ret, frame = cap.read()
     if ret:
-        dewarp_img = dewarp(frame, 685, 315, 776, 883) # 1280*720
+        #dewarp_img = dewarp(frame, 685, 315, 776, 883) # Image tuning
+	dewarp_img = dewarp(frame, 665, 315, 785, 870)
+        dewarp_img = cv2.resize(dewarp_img, (1280, 480))
+	
         cv2.imwrite(filename, dewarp_img)
-    	h, w, l = dewarp_img.shape
-    	print(w, h)
+    	#h, w, l = dewarp_img.shape
+    	#print(w, h)
 	out.write(dewarp_img)
     	time.sleep(1)
 
@@ -74,4 +80,4 @@ while(True):
         break 
  
 out.release()
-
+cap.release()
